9e9eece8d416712389c4b51d2a6867bee1784707
Kane <omgsif@gmail.com>
2014-01-21 14:56:09 +1100

    Production config

--- status/wsgi.py
+++ status/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 from __future__ import unicode_literals
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "status.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "status.settings.production")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
